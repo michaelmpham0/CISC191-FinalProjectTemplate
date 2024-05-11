25d8ceb55924fc160a46166b07bf7b3acdb07cc2
Phu <phugiaduong12@gmail.com>
2024-05-10 20:32:02 -0700

    Stuff

--- Server/src/main/java/LeaderboardApplication/LeaderboardApplication/src/main/java/edu/sdccd/cisci191/template/LeaderboardApplication/LeaderboardApplication.java
+++ Server/src/main/java/LeaderboardApplication/LeaderboardApplication/src/main/java/edu/sdccd/cisci191/template/LeaderboardApplication/LeaderboardApplication.java
@@ -1,7 +1,10 @@
 package edu.sdccd.cisci191.template.LeaderboardApplication;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 //Run this first for H2 databases to work
 @SpringBootApplication
@@ -11,4 +14,7 @@ public class LeaderboardApplication {
 		SpringApplication.run(LeaderboardApplication.class, args);
 	}
 
+	public Server inMemoryDBServer( ) throws SQLException{
+		return Server.createTcpServer("tcp","-tcpAllowOthers","tcpPort","5432");
+	}
 }
